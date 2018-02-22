1bf11399702275054129aac841f55970fc299caa
saroko-dnd <isaroko@mail.ru>
2018-02-22 17:18:41 +0200

    function was successfully implemented

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-// PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+function makeExchange(currency) {
+    const exchangeResult = {};
+
+    if (currency > 10000) {
+        exchangeResult.error =
+            "You are rich, my friend! We don't have so much coins for exchange";
+    } else if (currency > 0) {
+        makeExchange.coins.forEach(coin => {
+            if (currency >= coin.denomination) {
+                exchangeResult[coin.name] = Math.trunc(
+                    currency / coin.denomination
+                );
+            }
+            currency %= coin.denomination;
+        });
+    }
+
+    return exchangeResult;
 }
+
+makeExchange.coins = [
+    { name: "H", denomination: 50 },
+    { name: "Q", denomination: 25 },
+    { name: "D", denomination: 10 },
+    { name: "N", denomination: 5 },
+    { name: "P", denomination: 1 }
+];
+
+// PLEASE DON'T change function name
+module.exports = makeExchange;
